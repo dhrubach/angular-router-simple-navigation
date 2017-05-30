b260bf2ab96c5724141d713403f2916660869cf9
Dhruba Chatterjee <dhrubach@hotmail.com>
2017-05-30 11:36:10 +0530

    update(webpack) : import-loader to resolve jquery error

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 const webpack = require('webpack');
 const path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const ContextReplacementPlugin = require('webpack/lib/ContextReplacementPlugin');
+const ProvidePlugin = require('webpack/lib/ProvidePlugin');
+
 const config = {
 	entry: './src/main.ts',
 	output: {
@@ -20,9 +25,7 @@ const config = {
 			{
 				test: /\.ts$/,
 				exclude: /node_modules/,
-				use: [
-					{ loader: 'tslint-loader'}
-				],
+				loader: 'tslint-loader',
 				enforce: 'pre',
 				options: {
 					formattersDirectory: 'node_modules/custom-tslint-formatters/formatters',
@@ -32,30 +35,71 @@ const config = {
 			{
 				test: /\.js$/,
 				exclude: /node_modules/,
-				use:[
-					{ loader: 'babel-loader' }
-				]
+				use: 'babel-loader'
+			},
+			{
+				test: /bootstrap\/dist\/js$/,
+				use: 'imports-loader?jQuery=jquery'
 			},
 			{
 				test: /\.html$/,
 				exclude: /node_modules/,
-				use: [
-					{ loader: 'raw-loader' }
-				]
+				use: 'raw-loader'
 			},
 			{
 				test: /\.css$/,
-				exclude: /node_modules/,
-				use: [
-					{ loader: 'style-loader' },
-					{ loader: 'css-loader' }
-				]
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: 'css-loader'
+				})
+			},
+			{
+				test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+				loader: 'url-loader',
+				options: {
+					limit: 10000
+				}
+			},
+			{
+				test: /\.(ttf|eof|svg)(\?[\s\S]+)?$/,
+				use: 'file-loader'
 			}
 		]
 	},
-	plugins: [],
+	plugins: [
+		new webpack.optimize.CommonsChunkPlugin({
+			name: 'vendor',
+			minChunks: function (module) {
+				return module.context && module.context.indexOf('node_modules') > -1;
+			}
+		}),
+		new webpack.optimize.CommonsChunkPlugin({
+			name: 'manifest'
+		}),
+		new HtmlWebpackPlugin({
+			template: './src/index.ejs',
+			title: 'Angular Router Navigation App',
+			filename: 'index.html'
+		}),
+		new ExtractTextPlugin({
+			filename: 'styles.css'
+		}),
+		new ContextReplacementPlugin(
+			// The (\\|\/) piece accounts for path separators in *nix and Windows
+			/angular(\\|\/)core(\\|\/)@angular/,
+			path.resolve(__dirname, 'src') // location of your Client
+		),
+		new ProvidePlugin({
+			$: 'jquery',
+			jQuery: 'jquery',
+			'window.jQuery': 'jquery',
+			tether: 'tether',
+			Tether: 'tether',
+			'window.Tether': 'tether',
+		})
+	],
 	resolve: {
-		extensions: [ '.ts', '.js']
+		extensions: ['.ts', '.js']
 	},
 	devtool: 'cheap-module-source-map'
 };
