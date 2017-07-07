821ace090f8855232e0f325e8850e8693337cdc4
Dhruba Chatterjee <dhrubach@hotmail.com>
2017-07-07 21:19:09 +0530

    update(route) : back button in detail component

--- src/components/course-detail/course-detail.component.ts
+++ src/components/course-detail/course-detail.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 import { Course } from './../../models/course.model';
 
@@ -7,18 +8,30 @@ import { Course } from './../../models/course.model';
 	selector: '[course-detail]',
 	template: require('./course-detail.template.html'),
 })
-export class CourseDetailComponent implements OnInit {
+export class CourseDetailComponent implements OnDestroy, OnInit {
 
 	private title: string;
 	private longDescription: string;
+	private resolvedDataSubscription: Subscription;
 
-	constructor(private route: ActivatedRoute) { }
+	constructor(private route: ActivatedRoute, private router: Router) { }
 
 	public ngOnInit(): void {
-		this.route.data
+		this.resolvedDataSubscription = this.route.data
 			.subscribe((data: { course: Course }) => {
 				this.title = data.course.title;
 				this.longDescription = data.course.longDescription;
 			});
+
+		// console.log(this.route.data && (this.route.data as BehaviorSubject<Data>).observers.length);
 	}
+
+	public ngOnDestroy(): void {
+		this.resolvedDataSubscription.unsubscribe();
+	}
+
+	private navigateBack(): void {
+		this.router.navigate(['/courses']);
+	}
+
 }
